ad176f5279209cb92316697486bf38b6e14b778b
jonathan <joncanales2@gmail.com>
2023-08-04 15:33:36 -0400

    debugged and cleaned up resource deployment.

--- src/__checks__/json-checks.check.ts
+++ src/__checks__/json-checks.check.ts
@@ -19,7 +19,6 @@ const apps = JSON.parse(data);
 
 apps.forEach((app: any, index: number) => {
   // Create a new dashboard for each app
-  console.log(app.appName)
   const dashboard = new Dashboard(`${app.appName}-dashboard-${index + 1}`, {
     header: `${app.appName} Dashboard`,
     description: 'Dashboard associated with a basic demo',
@@ -30,42 +29,44 @@ apps.forEach((app: any, index: number) => {
 
   ['info', 'high', 'critical'].forEach((category) => {
     // Create a new check group for each category in the app
-    if (category.length > 0 ) {
-      const group = new CheckGroup(`${app.appName}-${category}-group`, {
-      name: `${app.appName} ${category} Group`,
-      activated: true,
-      muted: false,
-      runtimeId: '2023.02',
-      frequency: 1,
-      locations: ['us-east-1'],
-      tags: [app.appName, category],
-      environmentVariables: [],
-      apiCheckDefaults: {},
-      concurrency: 100,
-      alertChannels, // This variable needs to be defined somewhere
-    });
+    console.log(`${app.appName}, ${category}, ${app[category].length}`);
 
-    app[category].forEach((check: any, checkIndex: number) => {
-      // Create a new API check for each check in the category
-      new ApiCheck(`${app.appName}-${category}-api-check-${checkIndex + 1}`, {
-        name: `API - ${app.appName} ${category} ${check.urlShort} `,
-        group: group,
-        activated: check.activated,
-        tags: ['API', app.appName, category],
-        degradedResponseTime: 10000,
-        maxResponseTime: 20000,
-        setupScript: {
-          entrypoint: path.join(__dirname, './utils/setup.ts'),
-        },
-        request: {
-          url: check.url,
-          method: check.method,
-          followRedirects: true,
-          skipSSL: false,
-          assertions: [AssertionBuilder.statusCode().equals(200)],
-        },
+    if (app[category].length >= 1) {
+      const group = new CheckGroup(`${app.appName}-${category}-group`, {
+        name: `${app.appName} ${category} Group`,
+        activated: true,
+        muted: false,
+        runtimeId: '2023.02',
+        frequency: 1,
+        locations: ['us-east-1', 'us-west-2'],
+        tags: [app.appName, category],
+        environmentVariables: [],
+        apiCheckDefaults: {},
+        concurrency: 100,
+        alertChannels, // This variable needs to be defined somewhere
       });
-    });
+
+      app[category].forEach((check: any, checkIndex: number) => {
+        // Create a new API check for each check in the category
+        new ApiCheck(`${app.appName}-${category}-api-check-${checkIndex + 1}`, {
+          name: `API - ${app.appName} ${category} ${check.urlShort} `,
+          group: group,
+          activated: check.activated,
+          tags: ['API', app.appName, category],
+          degradedResponseTime: 10000,
+          maxResponseTime: 20000,
+          setupScript: {
+            entrypoint: path.join(__dirname, './utils/setup.ts'),
+          },
+          request: {
+            url: check.url,
+            method: check.method,
+            followRedirects: true,
+            skipSSL: false,
+            assertions: [AssertionBuilder.statusCode().equals(200)],
+          },
+        });
+      });
     }
   });
 });
