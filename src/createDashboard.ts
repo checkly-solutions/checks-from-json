9fcdf3683c0fd1ba23ebb5c968433fbf798fe5da
jonathan <joncanales2@gmail.com>
2023-09-20 12:57:59 -0400

    updated dashboard functionality to include incidents and be private by default. also added app names that team uses currently.

--- src/createDashboard.ts
+++ src/createDashboard.ts
@@ -6,9 +6,10 @@ export function createDashboard(appName: string, index: number) {
   return new Dashboard(`${formattedAppName}-dashboard-${index + 1}`, {
     header: `${appName} Dashboard`,
     description: 'Dashboard associated with a basic demo',
-    tags: [formattedAppName, 'cli'],
+    tags: [formattedAppName],
     useTagsAndOperator: false,
     isPrivate: true,
+    enableIncidents: true,
     logo: 'https://www.moviemaker.com/wp-content/uploads/2022/05/Could-Any-Other-Actor-Play-Himself-as-Well-as-Nicolas-Cage-Plays-Nicolas-Cage-675x444.jpg',
     customUrl: `${formattedAppName}-dashboard-${index + 1}`,
   });
